32beebc334be525896e1959cbaa39008288d5e65
Gerald Zhang <gerald_zhy@outlook.com>
2026-04-09 15:22:29 +0800

    chore: 调整全局字体

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,16 @@
 import { RootProvider } from 'fumadocs-ui/provider/next';
 import './global.css';
-import { Inter, JetBrains_Mono } from 'next/font/google';
+import { JetBrains_Mono } from 'next/font/google';
 import { cn } from "@/lib/utils";
-
-const inter = Inter({
-  subsets: ['latin'],
-  variable: '--font-sans',
-});
 
 const jetbrainsMono = JetBrains_Mono({
   subsets: ['latin'],
-  variable: '--font-mono',
+  variable: '--next-font-mono',
 });
 
 export default function Layout({ children }: LayoutProps<'/'>) {
   return (
-    <html lang="en" className={cn(inter.variable, jetbrainsMono.variable)} suppressHydrationWarning>
+    <html lang="en" className={cn(jetbrainsMono.variable)} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
       </body>
