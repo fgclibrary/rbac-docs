d96d8659b1069a1d1b59b92c0e3c7992871f686f
Gerald Zhang <gerald_zhy@outlook.com>
2026-04-27 20:49:13 +0800

    feat: 新增 formula 的高亮支持

--- source.config.ts
+++ source.config.ts
@@ -1,6 +1,7 @@
 import { remarkSteps } from "fumadocs-core/mdx-plugins/remark-steps";
 import { metaSchema, pageSchema } from "fumadocs-core/source/schema";
 import { defineConfig, defineDocs } from "fumadocs-mdx/config";
+import formula from "./lib/languages/formula";
 
 // You can customise Zod schemas for frontmatter and `meta.json` here
 // see https://fumadocs.dev/docs/mdx/collections
@@ -21,5 +22,18 @@ export default defineConfig({
   mdxOptions: {
     // MDX options
     remarkPlugins: [remarkSteps],
+    rehypeCodeOptions: {
+      themes: {
+        light: "github-light",
+        dark: "github-dark",
+      },
+      langs: [formula],
+      icon: {
+        extend: {
+          formula:
+            '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-square-function-icon lucide-square-function"><rect width="18" height="18" x="3" y="3" rx="2" ry="2"/><path d="M9 17c2 0 2.8-1 2.8-2.8V10c0-2 1-3.3 3.2-3"/><path d="M9 11.2h5.7"/></svg>',
+        },
+      },
+    },
   },
 });
